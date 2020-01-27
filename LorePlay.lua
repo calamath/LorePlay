1569abb7d0db8ddf08b0136f37b9a76417a0320e
Calamath <calamath-2t@anet.opal.ne.jp>
2020-01-27 18:10:59 +0900

    V1.6.51
    Version 1.6.51 : by Calamath
    - Fixed a bug that UI error occurs when LibDebugLogger is not installed.

--- LorePlay.lua
+++ LorePlay.lua
@@ -1,7 +1,7 @@
 LorePlay = LorePlay or {}
 LorePlay.majorVersion = 1
 LorePlay.minorVersion = 6
-LorePlay.bugVersion = 50
+LorePlay.bugVersion = 51
 LorePlay.version = LorePlay.majorVersion.."."..LorePlay.minorVersion.."."..LorePlay.bugVersion
 LorePlay.name = "LorePlay"
 LorePlay.authority = {2973583419,210970542} 
@@ -12,9 +12,10 @@ local function loreplayConfigDebug(arg)
 	local debugMode = false
 	local key = HashString(GetDisplayName())
 	local dummy = function() end
-	if not LibDebugLogger then return end
-	for _, v in pairs(arg or LorePlay.authority or {}) do
-		if key == v then debugMode = true end
+	if LibDebugLogger then
+		for _, v in pairs(arg or LorePlay.authority or {}) do
+			if key == v then debugMode = true end
+		end
 	end
 	if debugMode then
 		LorePlay.LDL = LibDebugLogger(LorePlay.name)
