7890b77044cd2a32eddbfe2d11718d7691d35cde
Calamath <calamath-2t@anet.opal.ne.jp>
2020-05-26 15:40:47 +0900

    V1.6.75
    - Final tweaks for release.

--- LorePlay.lua
+++ LorePlay.lua
@@ -1,7 +1,7 @@
 LorePlay = LorePlay or {}
 LorePlay.majorVersion = 1
 LorePlay.minorVersion = 6
-LorePlay.bugVersion = 74
+LorePlay.bugVersion = 75
 LorePlay.version = LorePlay.majorVersion.."."..LorePlay.minorVersion.."."..LorePlay.bugVersion
 LorePlay.name = "LorePlay"
 LorePlay.savedVars = "LorePlaySavedVars"
