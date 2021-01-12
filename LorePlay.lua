8b6ef4c93b58c42e2bbe9d132c20fcdae6a02e8a
Calamath <calamath-2t@anet.opal.ne.jp>
2021-01-12 12:59:06 +0900

    V1.6.81
    [HotFix]
    - Fixed a bug that the in-game furniture preview feature did not work when using the Camera Spin Disabler.

--- LorePlay.lua
+++ LorePlay.lua
@@ -1,7 +1,7 @@
 LorePlay = LorePlay or {}
 LorePlay.majorVersion = 1
 LorePlay.minorVersion = 6
-LorePlay.bugVersion = 80
+LorePlay.bugVersion = 81
 LorePlay.version = LorePlay.majorVersion.."."..LorePlay.minorVersion.."."..LorePlay.bugVersion
 LorePlay.name = "LorePlay"
 LorePlay.savedVars = "LorePlaySavedVars"
